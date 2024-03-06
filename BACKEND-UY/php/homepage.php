3d3c7660c05eab534b70687796b368a5559118d5
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-03-06 23:46:51 +0800

    Just added the fonts and created php for Home page

---
+++ BACKEND-UY/php/homepage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthy Foods</title>
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Cedarville+Cursive&family=Satisfy&display=swap');
+  
+    @import url('https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap');
+
+    .welcome-container {
+        width: max-content;
+        font-family: "Satisfy", cursive;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 50px;
+    }
+
+    .name-container {
+        font-family: "Luckiest Guy", cursive;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 100px;
+    }
+</style>
+<body>
+    <div class="welcome-container">
+            Welcome to
+            <div class="name-container">
+                Healthy Foods
+            </div>
+    </div>
+</body>
+</html>
