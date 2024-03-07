65b9c99e66269a2d47b82df3d9a1acee1da072f9
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-03-07 16:25:22 +0800

    Homepage UI done

--- BACKEND-UY/php/homepage.php
+++ BACKEND-UY/php/homepage.php
@@ -10,27 +10,63 @@
   
     @import url('https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap');
 
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
     .welcome-container {
         width: max-content;
         font-family: "Satisfy", cursive;
         font-weight: 400;
         font-style: normal;
-        font-size: 50px;
+        font-size: 3vw;
+        text-align: center;
+        margin: auto;
+        color: white;
+        padding-top: 3%;
     }
 
     .name-container {
         font-family: "Luckiest Guy", cursive;
         font-weight: 400;
         font-style: normal;
-        font-size: 100px;
+        font-size: 5vw;
+        text-align: center;
     }
+
+    button {
+        background-color: #311712;
+        border: none;
+        color: white;
+        font-family: "Montserrat", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 800;
+        font-style: normal;
+        font-size: 2vw;
+        padding: 1%;
+    }
+
+    .button-container {
+        position: absolute;
+        left: 65%;
+        bottom: 8%;
+        width: 35%;
+    }
+
+    body {
+        background-image: url("../images/homepage_background.png");
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
 </style>
 <body>
     <div class="welcome-container">
             Welcome to
-            <div class="name-container">
-                Healthy Foods
-            </div>
+        <div class="name-container">
+                Healthy Food
+        </div>
+        <div class="button-container">
+            <button type="button">CLICK HERE TO ORDER</button>
+        </div>
     </div>
 </body>
 </html>
