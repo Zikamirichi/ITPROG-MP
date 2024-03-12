a0a9bf34456e02f4d9942ae292d63864ea971aac
JyopsZ <timmyyap39@gmail.com>
2024-03-12 15:29:45 +0800

    OR Dispensing on paid.php
    After a transaction, OR number is now generated and displayed.

--- BACKEND-UY/paid.php
+++ BACKEND-UY/paid.php
@@ -1,5 +1,24 @@
 <?php
     session_start();
+    $cartID = $_SESSION['cartID'];
+
+    //CHANGE $CONN VARIABLES DEPENDING ON PERSONAL DEVICE SETTINGS
+    $conn = mysqli_connect("localhost", "root", "") or die ("Unable to connect!". mysqli_error($conn) );
+    mysqli_select_db($conn, "mydb");
+
+    // Generate OR NUM
+    $maxORQuery = mysqli_query($conn, "SELECT MAX(CAST(SUBSTRING(OR_num, 3) AS UNSIGNED)) AS maxOR_ FROM or_");
+    $row = mysqli_fetch_array($maxORQuery);
+    $maxOR = $row['maxOR_'];
+
+    $newOR = 'or' . ($maxOR + 1); // New OR Number
+
+    // Insert to DB
+    $insertORQuery = "INSERT INTO or_ (OR_num, cart_id) VALUES ('$newOR', '$cartID')";
+    mysqli_query($conn, $insertORQuery);
+
+    echo "OR Number: ". $newOR;
+
     session_destroy();
 ?>
-<a href="homepage.php">Back To Start</a>
+<br><br><a href="homepage.php">Back To Start</a>
