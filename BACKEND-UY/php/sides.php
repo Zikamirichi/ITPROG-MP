10e2b3968d60b47a196e01f8dc1ed0a57b10365e
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-03-12 12:05:01 +0800

    - Added a css folder - added prompt messages images

--- BACKEND-UY/php/sides.php
+++ BACKEND-UY/php/sides.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="navbar.css" />
-    <link rel="stylesheet" type="text/css" href="dish.css" />
+    <link rel="stylesheet" type="text/css" href="../css/navbar.css" />
+    <link rel="stylesheet" type="text/css" href="../css/dish.css" />
     <title>Side Dishes</title>
     <style>
         body {
