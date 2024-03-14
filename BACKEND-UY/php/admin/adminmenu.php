e53b5f89cb12927fdaf5bd6b0c8145f66e8029f6
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-03-14 15:42:18 +0800

    created a folder for nutrifacts table also changed href links

--- BACKEND-UY/php/admin/adminmenu.php
+++ BACKEND-UY/php/admin/adminmenu.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../admin/admin.css" />
+    <link rel="stylesheet" type="text/css" href="/ITPROG-MP/BACKEND-UY/css/admin.css" />
     <title>Main Menu</title>
     <style>
         .instructions {
@@ -19,13 +19,13 @@
 <body>
     <div class="main-container">
     <div class="header">
-      <img src="../images/logo-only.png" alt="Logo">
+      <img src="/ITPROG-MP/BACKEND-UY/images/logo-only.png" alt="Logo">
       MAIN MENU
     </div>
     <div class="content-box">
         <div class="instructions"> Please select a table:</div>
         <div class="buttons-box">
-            <a href="../admin/modnutrfacts.php" class="button-table">Nutrition Facts Table</a>
+            <a href="../NutriFacts/modnutrfacts.php" class="button-table">Nutrition Facts Table</a>
             <br> 
             <a href="../Main/mains-table.php" class="button-table">Mains Table</a>
             <br> 
