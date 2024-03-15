21b7c0134df7550a77bc35a0e8c402eff96be222
virtuozogit <125036415+virtuozogit@users.noreply.github.com>
2024-03-15 08:02:12 +0800

    added insertSidesXML

---
+++ BACKEND-UY/php/admin/sides-table/insertSidesXMLtoDB.php
@@ -0,0 +1,35 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "mydb");
+
+$affectedRow = 0;
+
+$xml = simplexml_load_file("sides.xml") or die("Error: Cannot create object");
+
+foreach ($xml->children() as $row) {
+  echo $sides_id = $row->sides_id;
+  echo $name = $row->name;
+  echo $price = $row->price;
+  echo $nutr_facts_id = $row->nutr_facts_id;
+  echo $stocks_id = $row->stocks_id;
+  echo "<br />";  
+    $sql = "INSERT INTO sides(sides_id, name, price, nutr_facts_id, stocks_id) VALUES ('$sides_id', '$name', '$price', '$nutr_facts_id', '$stocks_id')";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    if (!empty($result)) {
+        $affectedRow ++;
+    } else {
+        $error_message = mysqli_error($conn) . "\n";
+    }
+}
+?>
+<h2>Insert XML Data to MySql Table Output</h2>
+<?php
+if ($affectedRow > 0) {
+    $message = $affectedRow . " records inserted";
+} else {
+    $message = "No records inserted";
+}
+
+echo "<h3><font color='green'>" .$message."</font></h2>";
+?>
