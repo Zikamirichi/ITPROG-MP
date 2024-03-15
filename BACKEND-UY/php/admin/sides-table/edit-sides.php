958d539fbd30f2049910c8c8b8ae1c2ee8719cdb
virtuozogit <125036415+virtuozogit@users.noreply.github.com>
2024-03-15 17:59:01 +0800

    Update edit-sides.php

--- BACKEND-UY/php/admin/sides-table/edit-sides.php
+++ BACKEND-UY/php/admin/sides-table/edit-sides.php
@@ -1,6 +1,83 @@
 <html>
-<head><title>Using the Edit Statement</title></head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/ITPROG-MP/BACKEND-UY/css/admin.css" />
+    <title>Using Edit</title>
+    <style>
+        table {
+            border: 1px solid black;
+        }
+
+        td, th {
+            border: 1px solid black;
+            text-align: center;
+            background-color: white;
+        }
+
+        tr th {
+            background-color: #311712; 
+            color: white;
+        }
+
+        hr {
+            margin: 5%;
+        }
+
+    </style>
+</head>
+
 <body>
+    <!-- Sides table -->
+    <div class="main-container">
+        <div class="header">
+            <img src="/ITPROG-MP/BACKEND-UY/images/logo-only.png" alt="Logo">
+            SIDE DISH TABLE
+        </div>
+
+        <?php
+                error_reporting(E_ERROR | E_PARSE);
+                //CHANGE $CONN VARIABLES DEPENDING ON PERSONAL DEVICE SETTINGS
+                $conn = mysqli_connect("localhost", "root", "") or die ("Unable to connect!". mysqli_error($conn) );
+                mysqli_select_db($conn, "mydb");
+        ?>
+
+        <div class="content-box">
+            <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Nutrition Facts ID</th>
+                <th>Stock ID</th>
+            </tr>
+
+            <?php
+                $factsQuery = mysqli_query($conn, "SELECT * FROM sides ORDER BY sides_id");
+                while ($factsResult = mysqli_fetch_assoc($factsQuery)) {
+                    echo "<tr>";
+                    echo "<td>", $factsResult ["sides_id"], "</td>";
+                    echo "<td>", $factsResult ["name"], "</td>";
+                    echo "<td>", $factsResult ["price"], "</td>";
+                    echo "<td>", $factsResult ["nutr_facts_id"], "</td>";
+                    echo "<td>", $factsResult ["stocks_id"], "</td>";
+                    echo "</tr>";
+                }
+            ?>
+            </table>
+
+            <hr>
+            <div class="buttons-box">
+                <a href="add-sides.php" class="button-table">Add Sides</a> <br>
+                <a href="delnutrfacts.php" class="button-table">Delete Sides</a> <br>
+                <a href="editnutrfacts.php" class="button-table">Edit Sides</a> <br> <br>
+                <div class="back-submit-container">
+                    <a href="../adminmenu.php" class="back-button">Back</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
     <?php
         $conn = mysqli_connect("localhost", "root", "") or die ("Unable to connect!". mysqli_error($conn) );
         mysqli_select_db($conn, "mydb");
@@ -77,7 +154,7 @@
 
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     
-    Select Nutrition Facts ID: 
+    Select Sides ID: 
     <select name="sides_id">
         <?php
         $idQuery = mysqli_query($conn, "SELECT sides_id FROM sides");
