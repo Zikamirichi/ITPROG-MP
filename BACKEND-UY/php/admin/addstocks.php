bc51c9f7fc08116660fe695af8987efe066c11f8
AdrianVillador <160464205+AdrianVillador@users.noreply.github.com>
2024-03-27 14:45:39 +0800

    Adding Data With Prompt
    Success Prompt after a successful adding of data

--- BACKEND-UY/php/admin/addstocks.php
+++ BACKEND-UY/php/admin/addstocks.php
@@ -3,7 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/ITPROG-MP/BACKEND-UY/css/admin.css" />
+    <!-- jQuery and Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <title>Stocks</title>
 </head>
 <body>
     <hr>
@@ -27,6 +34,40 @@
         </tr>
         <tr><td colspan='2'><input type='submit' value='Save' name='save' /></td></tr>
     </table>
+
+    <?php
+        ob_start(); // Start buffering output
+    ?>
+
+    <!-- Modal for Add Data Success -->
+    <div class="modal fade <?php if(isset($_POST['save'])) {echo 'show d-block';} ?>" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" style="background: rgba(0, 0, 0, 0.5);">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Success!</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Record has been successfully inserted!
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <script>
+        // Script to manually close the modal
+        $(document).ready(function() {
+            $(".close, .btn-secondary").click(function() {
+                $("#exampleModalCenter").removeClass("show d-block").hide();
+            });
+            
+        });
+    </script>
 
     <?php
         error_reporting(E_ERROR | E_PARSE);
@@ -68,6 +109,10 @@
         }
     ?>
 
+    <?php
+        ob_end_flush(); // Send output and turn off buffering
+    ?>
+    
     <a href="modstocks.php">Back</a>
 </body>
 </html>
