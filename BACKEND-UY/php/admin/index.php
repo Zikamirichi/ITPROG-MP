b41cca9c0fe5480a5f0e963999a87c6cb55b76ba
Nek0Kam1 <160472121+Nek0Kam1@users.noreply.github.com>
2024-03-28 16:29:56 +0800

    Update #2 index.php

--- BACKEND-UY/php/admin/index.php
+++ BACKEND-UY/php/admin/index.php
@@ -3,8 +3,6 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
    <link rel="stylesheet" type="text/css" href="../../css/admin.css" />
    <title>Login</title>
    <style>
@@ -23,37 +21,16 @@
       <div class="content-box">
          <table>
          <tr>
-            <form method="post" action="check.php">
-               <td>Username:</td><td><input type="text" name="username" class="form-control" required/></td>
-               <tr><td>Password:</td><td>
-               <div class="input-group">
-                  <input type="password" id="password" name="password" class="form-control" required>
-                  <div class="input-group-append">
-                      <button class="btn btn-outline-secondary" type="button" id="togglePassword"><i class="fa fa-eye" aria-hidden="true"></i></button>
-                  </div>
-               </div>
-               </td></tr>
-               <tr><td colspan="2" class="submit-button"><input type="submit" value="Login" name="loginBtn" class="btn btn-primary"/></td>
+            <form id="loginForm" method="post" action="check.php">
+               <td>Username:</td><td><input type="text" name="username" required/></td>
+               <tr><td>Password:</td><td><input type="password" name="password" id="password" required/><input type="image" src="eye.png" alt="Eye" onclick="togglePasswordVisibility(event)" style="width: 20px; height: 20px;"></td></tr>
+               <tr><td colspan="2" class="submit-button"><input type="submit" value="Login" name="loginBtn"/></td>
             </form>
          </tr>
          </table>
       </div>
    </div>
-
-   <!-- JavaScript for password toggle -->
-   <script>
-      document.getElementById("togglePassword").addEventListener("click", function() {
-          var passwordField = document.getElementById("password");
-          if (passwordField.type === "password") {
-              passwordField.type = "text";
-              this.innerHTML = '<i class="fa fa-eye-slash" aria-hidden="true"></i>';
-          } else {
-              passwordField.type = "password";
-              this.innerHTML = '<i class="fa fa-eye" aria-hidden="true"></i>';
-          }
-      });
-   </script>
-
+   
    <?php
       if(isset($_GET["error"])) {
           $error=$_GET["error"];
@@ -63,6 +40,19 @@
             echo "<p align='center'>Username and/or password invalid<br/></p>"; 
 		   }
       }
+ 
    ?>
+   <script>
+      function togglePasswordVisibility(event) {
+         event.preventDefault();
+         var passwordField = document.getElementById("password");
+
+         if (passwordField.type === "password") {
+            passwordField.type = "text";
+         } else {
+            passwordField.type = "password";
+         }
+      }
+   </script>
 </body>
 </html>
