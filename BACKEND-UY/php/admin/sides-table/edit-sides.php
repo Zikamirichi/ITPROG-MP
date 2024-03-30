920647fb85b42c9502293c1a82ff26f0a5fad646
Nek0Kam1 <160472121+Nek0Kam1@users.noreply.github.com>
2024-03-31 02:26:07 +0800

    Updated edit-sides.php

--- BACKEND-UY/php/admin/sides-table/edit-sides.php
+++ BACKEND-UY/php/admin/sides-table/edit-sides.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../../css/admin.css" />
-    <title>Using Edit</title>
+    <title>Side Dish</title>
     <style>
         table {
             border: 1px solid black;
@@ -43,13 +43,13 @@
     <div class="main-container">
         <div class="header">
             <img src="../../../images/logo-only.png" alt="Logo">
-            SIDE DISH TABLE
+            EDIT SIDE DISH
         </div>
 
         <?php
                 error_reporting(E_ERROR | E_PARSE);
                 //CHANGE $CONN VARIABLES DEPENDING ON PERSONAL DEVICE SETTINGS
-                $conn = mysqli_connect("localhost", "root", "") or die ("Unable to connect!". mysqli_error($conn) );
+                $conn = mysqli_connect("localhost", "root", "", "mydb") or die ("Unable to connect!". mysqli_error($conn) );
                 mysqli_select_db($conn, "mydb");
         ?>
 
@@ -60,29 +60,28 @@
                 <th>Name</th>
                 <th>Price</th>
                 <th>Nutrition Facts ID</th>
-                <th>Stock ID</th>
+                <th>Stocks</th>
             </tr>
 
             <?php
-                $factsQuery = mysqli_query($conn, "SELECT * FROM sides ORDER BY sides_id");
+                $factsQuery = mysqli_query($conn, "SELECT si.*, s.quantity 
+                FROM sides si JOIN stocks s
+                             ON   si.stocks_id = s.stocks_id
+                 ORDER BY sides_id;");
                 while ($factsResult = mysqli_fetch_assoc($factsQuery)) {
                     echo "<tr>";
                     echo "<td>", $factsResult ["sides_id"], "</td>";
                     echo "<td>", $factsResult ["name"], "</td>";
                     echo "<td>", $factsResult ["price"], "</td>";
                     echo "<td>", $factsResult ["nutr_facts_id"], "</td>";
-                    echo "<td>", $factsResult ["stocks_id"], "</td>";
+                    echo "<td>", $factsResult ["quantity"], "</td>";
                     echo "</tr>";
                 }
             ?>
             </table>
-
-            <hr>
-        </div>
-    </div>
     
     <?php
-        $conn = mysqli_connect("localhost", "root", "") or die ("Unable to connect!". mysqli_error($conn) );
+        $conn = mysqli_connect("localhost", "root", "", "mydb") or die ("Unable to connect!". mysqli_error($conn) );
         mysqli_select_db($conn, "mydb");
 
         if(isset($_POST["enter"])){
@@ -98,7 +97,8 @@
             $id = $getSidesInfo['nutr_facts_id'];
             $factsQuery = mysqli_query($conn, "SELECT * FROM nutr_facts WHERE nutr_facts_id='$id'");
             $getFacts = mysqli_fetch_array($factsQuery);
-            echo "<form method='post' action='".$_SERVER['PHP_SELF']."'>";
+            
+            echo "<form method='post' action='".$_SERVER['PHP_SELF']."' enctype='multipart/form-data'>";
             // sides info
             echo "<h3>Sides info</h3>";
             echo "<input type='hidden' name='newMainID' value='".$getSidesInfo["sides_id"]."'>".$getSidesInfo["sides_id"]."<br />";
@@ -108,46 +108,85 @@
             // Nutrition facts
             echo "<h3>Nutrition facts</h3>";
             echo "<input type='hidden' name='newID' value='".$getFacts["nutr_facts_id"]."'>".$getFacts["nutr_facts_id"]."<br />";
-            echo "Description: <input type='text' name='newDesc' value='".$getFacts["desc"]."' size='150'> <br />";
-            echo "Ingredients: <input type='text' name='newIngredients' value='".$getFacts["Ingredients"]."' size='100'> <br />";
-            echo "Fat: <input type='text' name='newFat' value='".$getFacts["Fat"]."'><br />";
-            echo "Calories: <input type='text' name='newCalories' value='".$getFacts["Calories"]."'><br />";
-            echo "Carbs: <input type='text' name='newCarbs' value='".$getFacts["Carbs"]."'><br />";
-            echo "Protein: <input type='text' name='newProtein' value='".$getFacts["Protein"]."'><br />";
+            echo "Description: <input type='text' name='newDesc' value='".$getFacts["desc"]."' size='10' placeholder='Maximum of 44 characters' maxlength='44'> <br />";
+            echo "Ingredients: <input type='text' name='newIngredients' value='".$getFacts["Ingredients"]."' size='10' placeholder='Maximum of 500 characters' maxlength='500'> <br />";
+            echo "Fat (g) : <input type='number' name='newFat' value='".$getFacts["Fat"]."' min='0' step='0.1'><br />";
+            echo "Calories (g) : <input type='number' name='newCalories' value='".$getFacts["Calories"]."' min='0' step='0.1'><br />";
+            echo "Carbs (g) : <input type='number' name='newCarbs' value='".$getFacts["Carbs"]."' min='0' step='0.1'><br />";
+            echo "Protein (g) : <input type='number' name='newProtein' value='".$getFacts["Protein"]."' min='0' step='0.1'><br />";
 
             // quantity info
             echo "<h3>Stocks info</h3>";
             echo "<input type='hidden' name='stocksID' value='".$getStocks["stocks_id"]."'>".$getStocks["stocks_id"]."<br />";
-            echo "Price: <input type='number' name='newQuantity' value='".$getStocks["quantity"]."' size='150'> <br />";
+            echo "Quantity: <input type='number' name='newQuantity' value='".$getStocks["quantity"]."' size='150'> <br />";
 
-            echo "<input type='submit' name='save' value='Save'><br />";
+            // Image
+            echo "<h3>Image</h3>";
+            echo '<img src="' . '../../../images/' . $getSidesInfo['image_name'] . '" width="50">';
+            echo "<br><br>";
+            echo '<input type="file" name="newImage">';
+
+            echo "<div class=save-box>";
+            echo "<input type='submit' name='save' value='Save'<br />";
+            echo "</div>";
             echo "</form>";
         }
 
         if(isset($_POST["save"])){
+
+            $mainID = $_POST['newMainID']; 
+            $stocksID = $_POST['stocksID'];
+            $id = $_POST['newID'];
+
             // nutr update
-            $newID = $_POST["newID"];
             $newDesc = $_POST["newDesc"];
             $newIngredients = $_POST["newIngredients"];
             $newFat = $_POST["newFat"];
             $newCalories = $_POST["newCalories"];
             $newCarbs = $_POST["newCarbs"];
             $newProtein = $_POST["newProtein"];
-            mysqli_query($conn, "UPDATE nutr_facts set `desc`='$newDesc', Ingredients='$newIngredients', Fat='$newFat', Calories='$newCalories', Carbs='$newCarbs', Protein='$newProtein'
-                                WHERE nutr_facts_id='$newID'");
 
             // stocks update
-            $stocksID = $_POST["stocksID"];
             $newQuantity = $_POST["newQuantity"];
-            mysqli_query($conn, "UPDATE stocks set `quantity`='$newQuantity'
-                                WHERE stocks_id='$stocksID'");
 
             // sides update
-            $newMainID = $_POST["newMainID"];
             $newName = $_POST["newName"];
             $newPrice = $_POST["newPrice"];
-            mysqli_query($conn, "UPDATE sides set `name`='$newName', `price`='$newPrice'
-                                WHERE sides_id='$newMainID'");
+            
+            // Handle image upload
+            if ($_FILES['newImage']['name']) {
+                $target_dir = __DIR__ . "/../../../images/";
+                $image = $_FILES['newImage']['name'];
+                $imageExt = pathinfo($image, PATHINFO_EXTENSION);
+
+                // Allow only .jpg files
+                if ($imageExt == "jpg") {
+                    
+                    $target_file = $target_dir . basename($image);
+                    move_uploaded_file($_FILES["newImage"]["tmp_name"], $target_file);
+
+                    // UPDATE STATEMENTS AFTER SUCCESSFUL IMAGE UPLOAD
+                    mysqli_query($conn, "UPDATE nutr_facts set `desc`='$newDesc', Ingredients='$newIngredients', Fat='$newFat', Calories='$newCalories', Carbs='$newCarbs', Protein='$newProtein'
+                    WHERE nutr_facts_id='$id'");
+                                
+                    mysqli_query($conn, "UPDATE stocks set `quantity`='$newQuantity'
+                    WHERE stocks_id='$stocksID'");
+                    
+                    // Update database with new image name
+                    mysqli_query($conn, "UPDATE sides set `name`='$newName', `price`='$newPrice', `image_name`='$image'
+                                        WHERE sides_id='$mainID'");
+                } 
+                
+                else {
+                    
+                    echo "Only .jpg files allowed, please try again.";
+                }
+            } 
+            
+            else {
+                
+                echo "Error uploading image";
+            }
 
             echo "Record has been updated!";
         }
@@ -155,24 +194,23 @@
 
     <hr>
 
+    <div class="select-box">
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    
-    Select Sides ID: 
+    Select ID: 
     <select name="sides_id">
         <?php
-        $idQuery = mysqli_query($conn, "SELECT sides_id FROM sides");
+        $idQuery = mysqli_query($conn, "SELECT * FROM sides");
 
         // Loop through the results and populate dropdown options
         while ($row = mysqli_fetch_assoc($idQuery)) {
-            echo "<option value='" . $row['sides_id'] . "'>" . $row['sides_id'] . "</option>";
+            echo "<option value='" . $row['sides_id'] . "'>" . $row['name'] . " (" . $row['sides_id'] . ")</option>";
         }
         ?>
     </select>
-
+    <br/><br/>
+    <a class="back-button" href="sides-table.php">Back</a>
     <input type="submit" name="enter" value="Enter" /><br /><br />
     </form>
-
-    <a class="back-button" href="sides-table.php">Back</a>
 
 </body>
 </html>
