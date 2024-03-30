ad4af19d91c6af8313fa68d0e2f6422ed9a7126c
virtuozogit <125036415+virtuozogit@users.noreply.github.com>
2024-03-30 10:32:16 +0800

    edited some stuff

--- BACKEND-UY/php/customer/mains.php
+++ BACKEND-UY/php/customer/mains.php
@@ -299,16 +299,11 @@
             
             while ($factsResult = mysqli_fetch_assoc($factsQuery)) {
                 
-                echo '<div class="dish-desc">';
-                echo "<strong>Nutrition Facts:</strong> <br>";
-                echo "Calories: ". $factsResult ["Calories"], "g<br>";
+                echo "Ingredients: ". $factsResult ["Ingredients"], "g<br><br>";
                 echo "Fat: ". $factsResult ["Fat"], "g<br>";
+                echo "Calories: ". $factsResult ["Calories"], "g<br>";
                 echo "Carbs: ". $factsResult ["Carbs"], "g<br>";
                 echo "Protein: ". $factsResult ["Protein"], "g<br>";
-
-                echo '<br>';
-                echo "Ingredients: <br>". $factsResult ["Ingredients"], "<br><br>";
-                echo '</div>';
             }
         }
     ?>
