1100d1c99d1b82113a06e484f8edfcd46c96efd1
aajrong342 <141275124+aajrong342@users.noreply.github.com>
2024-03-31 23:49:00 +0800

    Edit with Prompts

--- BACKEND-UY/php/admin/sides-table/edit-sides.php
+++ BACKEND-UY/php/admin/sides-table/edit-sides.php
@@ -2,6 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../../../css/admin.css" />
     <title>Side Dish</title>
     <style>
@@ -98,7 +104,7 @@
             $factsQuery = mysqli_query($conn, "SELECT * FROM nutr_facts WHERE nutr_facts_id='$id'");
             $getFacts = mysqli_fetch_array($factsQuery);
             
-            echo "<form method='post' action='".$_SERVER['PHP_SELF']."' enctype='multipart/form-data'>";
+            echo "<form id='saveForm' method='post' action='".$_SERVER['PHP_SELF']."' enctype='multipart/form-data'>";
             // sides info
             echo "<h3>Sides info</h3>";
             echo "<input type='hidden' name='newMainID' value='".$getSidesInfo["sides_id"]."'>".$getSidesInfo["sides_id"]."<br />";
@@ -126,10 +132,10 @@
             echo "<br><br>";
             echo '<input type="file" name="newImage">';
 
-            echo "<div class=save-box>";
-            echo "<input type='submit' name='save' value='Save'<br />";
-            echo "</div>";
-            echo "</form>";
+        echo "<div class='save-box'>";
+        echo "<button type='button' id='saveButton' class='submit-bttn'>Save</button><br />";
+        echo "</div>";
+        echo "</form>";
         }
 
         if(isset($_POST["save"])){
@@ -211,6 +217,69 @@
     <a class="back-button" href="sides-table.php">Back</a>
     <input type="submit" name="enter" value="Enter" /><br /><br />
     </form>
+    </div>
+    
+    <!-- Modal -->
+<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to save the changes?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Back</button>
+                <button type="button" id="confirmSaveButton" class="submit-bttn">Save Changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- New Modal for Success Alert -->
+<div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="successModalLabel">Success</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Save successful!
+            </div>
+            <div class="modal-footer">
+                <button type="button" id="successModalButton" class="submit-bttn" data-dismiss="modal">OK</button>
+            </div>
+        </div>
+    </div>
+</div>
 
+<!-- Updated Script -->
+<script>
+    $(document).ready(function() {
+        // Show confirmation modal when the Save button is clicked
+        $("#saveButton").click(function() {
+            // Show the modal
+            $('#confirmationModal').modal('show');
+        });
+
+        // Handle Save button click inside the modal
+        $("#confirmSaveButton").click(function() {
+            // Show success modal after form submission
+            $('#confirmationModal').modal('hide');
+            $('#successModal').modal('show');
+        });
+
+        $("#successModalButton").click(function() {
+            // Submit the form with the name "save"
+            $("#saveForm").append("<input type='hidden' name='save' value='save'>").submit();
+        });
+    });
+</script>
 </body>
 </html>
