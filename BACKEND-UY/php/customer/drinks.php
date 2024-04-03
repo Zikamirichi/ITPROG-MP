b1954929763ea59d876718d1f25f5182ab2737f0
Nek0Kam1 <160472121+Nek0Kam1@users.noreply.github.com>
2024-04-03 20:40:59 +0800

    Update drinks.php

--- BACKEND-UY/php/customer/drinks.php
+++ BACKEND-UY/php/customer/drinks.php
@@ -15,7 +15,7 @@
             background-position: center;
             display: flex;
         }
-
+        
         .hidden-div {
             display: none;
             position: absolute;
@@ -24,24 +24,46 @@
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            background-color: lightgrey;
+            background-color: white;
             padding: 20px;
+            padding-bottom: 30px;
+            border: 1px solid black;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             text-align: center;
         }
+        
 
         h2 {
             padding: 25px;
             margin: 0;
-            margin-bottom: 30px;
+            margin-bottom: 20px;
+            width: 525px;
             background-color: #D4471F;
             font-family: "Luckiest Guy", cursive;
             font-weight: 400;
             font-style: normal;
-            font-size: 50px;
+            font-size: 40px;
             color: white;
         }
+
+        /* CSS styles for the quantity modifier box */
+        .counter {
+            margin-top: 10px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%; 
+        }
+
+        .counter input[type="number"] {
+            width: 90px; 
+            height:30px;
+            font-size: 25px;
+            text-align: center;
+            margin-bottom: 10px; 
+        }
+
     </style>
 </head>
 <body>
@@ -92,176 +114,185 @@
     <div class="right-container">
         <div class="select-text">DRINKS</div>
             
-            <div class="dish-main-box">
-                <?php
-                    $drinksQuery = mysqli_query($conn, "SELECT * FROM drinks"); // Fetch all drinks dishes
-                    while ($drinkItem = mysqli_fetch_assoc($drinksQuery)) { // Iterate over each drinks
-                ?>
-                <div class="dish-item item" onclick="showHiddenDiv('<?php echo $drinkItem['drinks_id']; ?>')"> <!-- Onclick for hidden div -->
-                    <div class="dish-img-box">
-                        <?php
-                            echo "<img src='../../images/" . $drinkItem['image_name'] . "' alt='" . $drinkItem['names'] . "'>";
-                        ?>
-                    </div>
-
-                    <div class="dish-title"><?php echo $drinkItem['names']; ?></div>
-                    <div class="dish-desc"><?php echo getDrinkDesc($conn, $drinkItem['nutr_facts_id']); ?></div>
-                    <div class="dish-price"><?php echo "PHP " . $drinkItem['price'] . ".00"; ?></div>
-
-                    <span class="facts">
-                        <?php echo getDrinkFacts($conn, $drinkItem['nutr_facts_id']); ?>
-                    </span>
+        <div class="dish-main-box">
+            <?php
+                $drinksQuery = mysqli_query($conn, "SELECT * FROM drinks"); // Fetch all drinks dishes
+                while ($drinkItem = mysqli_fetch_assoc($drinksQuery)) { // Iterate over each drinks
+            ?>
+            <div class="dish-item item" onclick="showHiddenDiv('<?php echo $drinkItem['drinks_id']; ?>')"> <!-- Onclick for hidden div -->
+                <div class="dish-img-box">
+                    <?php
+                        echo "<img src='../../images/" . $drinkItem['image_name'] . "' alt='" . $drinkItem['names'] . "'>";
+                    ?>
                 </div>
-                <?php
-                    }?>
+
+                <div class="dish-title"><?php echo $drinkItem['names']; ?></div>
+                <div class="dish-desc"><?php echo getDrinkDesc($conn, $drinkItem['nutr_facts_id']); ?></div>
+                <div class="dish-price"><?php echo "PHP " . $drinkItem['price'] . ".00"; ?></div>
+
+                <span class="facts">
+                    <?php echo getDrinkFacts($conn, $drinkItem['nutr_facts_id']); ?>
+                </span>
             </div>
+            <?php
+                }?>
         </div>
     </div>
+</div>
 
-    <?php
-        // Loop through all drinks in the table
-        $drinksResult = mysqli_query($conn, "SELECT * FROM drinks"); // Fetch all drinks
-        while ($row = mysqli_fetch_assoc($drinksResult)) {
+<?php
+    // Loop through all drinks in the table
+    $drinksResult = mysqli_query($conn, "SELECT * FROM drinks"); // Fetch all drinks
+    while ($row = mysqli_fetch_assoc($drinksResult)) {
 
-            $drinkID = $row['drinks_id'];
-            $name = $row['names'];
-            $stockID = $row['stocks_id'];
-            $imageName = $row['image_name'];
+        $drinkID = $row['drinks_id'];
+        $name = $row['names'];
+        $stockID = $row['stocks_id'];
+        $imageName = $row['image_name'];
+    
+        // Get max stock
+        $stockQuery = "SELECT quantity FROM stocks WHERE stocks_id='$stockID'";
+        $stockResult = mysqli_query($conn, $stockQuery);
+        $stockRow = mysqli_fetch_assoc($stockResult);
+        $maxStock = $stockRow['quantity'];
+    
+        // Hidden div
+        echo "<div id='$drinkID' class='hidden-div'>";
+            echo "<h2>$name</h2>";
+
+            // Limit style to 10%, may edit later
+            echo "<img src='../../images/$imageName' alt='" . $name . "' style='width: 300px; height: 200px; object-fit: cover;'>";
         
-            // Get max stock
-            $stockQuery = "SELECT quantity FROM stocks WHERE stocks_id='$stockID'";
-            $stockResult = mysqli_query($conn, $stockQuery);
-            $stockRow = mysqli_fetch_assoc($stockResult);
-            $maxStock = $stockRow['quantity'];
-        
-            // Hidden div
-            echo "<div id='$drinkID' class='hidden-div'>";
-                echo "<h2>$name</h2>";
-
-                // Limit style to 10%, may edit later
-                echo "<img src='../../images/$imageName' alt='" . $name . "' style='width: 300px; height: 250px; object-fit: cover;'>";
-            
-                echo "<div class='counter'>";
-                    echo "<form method='post'>";
-                        echo "<input type='hidden' name='item' value='$drinkID'>";
-                        echo "<input type='number' name='count' value='1' min='1' max='$maxStock'>";
-                        echo "<input type='submit' name='submit'>";
-                    echo "</form>";
-                echo "</div>";
+            echo "<div class='counter'>";
+                echo "<form method='post'>";
+                    echo "<input type='hidden' name='item' value='$drinkID'>";
+                    echo "<input type='number' name='count' value='1' min='1' max='$maxStock'>";
+                    echo "<div>"; // Start a div to contain both buttons
+                        echo "<button type='button' class='cancel-button' onclick=\"hideHiddenDiv('$drinkID')\">Cancel</button>"; // Cancel button
+                        echo "<button type='submit' class='submit-button' name='submit'>Submit</button>"; // Submit button
+                    echo "</div>"; // End of button div
+                echo "</form>";
             echo "</div>";
+        echo "</div>";
+    
+    }
+?>
+
+<script>
+    // Function to show the hidden div based on the provided div ID
+    function showHiddenDiv(divId) {
+        var divToShow = document.getElementById(divId);
+        if (divToShow.style.display === "none" || divToShow.style.display === "") {
+            
+            divToShow.style.display = "block";
+        } 
         
+        else {
+            
+            divToShow.style.display = "none";
         }
-  ?>
+    }
 
-    <script>
-        // Function to show the hidden div based on the provided div ID
-        function showHiddenDiv(divId) {
-            var divToShow = document.getElementById(divId);
-            if (divToShow.style.display === "none" || divToShow.style.display === "") {
-                
-                divToShow.style.display = "block";
-            } 
-            
-            else {
-                
-                divToShow.style.display = "none";
-            }
-        }
-    </script>
+    // Function to hide the hidden div based on the provided div ID
+    function hideHiddenDiv(divId) {
+        var divToHide = document.getElementById(divId);
+        divToHide.style.display = "none";
+    }
+</script>
 
-    <?php
-        if(!isset($_SESSION['drinks'])) { // Checks if a drinks array has already been set (to avoid reset everytime drinks.php is visited in current session)
-            
-            $_SESSION['drinks'] = []; // Set an array to store all drinks orders in SESSION
-        }
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") { // When a post is done above, execute code
-            
-            if(isset($_POST["count"])) {
-
-                $count = $_POST["count"];
-            
-                // Get max order ID
-                $maxOrderQuery = "SELECT MAX(CAST(SUBSTRING(ordr_id, 2) AS UNSIGNED)) AS max_orderID FROM order_table";
-                $maxResult = mysqli_query($conn, $maxOrderQuery);
-                $maxRow = mysqli_fetch_assoc($maxResult);
-                $maxOrderID = $maxRow['max_orderID'];
-            
-                $orderID = "o" . ($maxOrderID + 1);
-            
-                $drinkID = $_POST["item"]; // Get $drinkID
-            
-                $order = new Order($orderID); 
-                $order->setOrder($count, $drinkID); // Set new order
-              }
-            
-            
-            if(isset($_POST["submit"])) {
-                $ordr_id = $order->getOrderID();
-                $quantity = $order->getQuantity();
-                $item_id = $order->getItemID();
-            
-                error_reporting(E_ERROR | E_PARSE);
-                
-                $insert = "INSERT INTO order_table VALUES ('$ordr_id', '$quantity', '$item_id')";
-                mysqli_query($conn, $insert);
-
-                // Update stocks table based on item ID with order quantity
-                $updateDrinkStock = "UPDATE stocks s
-                                        JOIN drinks d ON s.stocks_id = d.stocks_id
-                                        SET s.quantity = s.quantity - $quantity
-                                        WHERE d.drinks_id = '$item_id'";
-                mysqli_query($conn, $updateDrinkStock); 
-
-                array_push($_SESSION['drinks'],$ordr_id); // Add order IDs to drinks array in session
-
-                echo "Record has been successfully inserted!";
-                
-                } else {
-                    echo "Failed to insert record!!!";
-                }
-        }
-
-        function getDrinkDesc($conn, $ntrID) {
+<?php
+    if(!isset($_SESSION['drinks'])) { // Checks if a drinks array has already been set (to avoid reset everytime drinks.php is visited in current session)
         
-            $factsQuery = mysqli_query($conn, "SELECT * FROM nutr_facts WHERE nutr_facts_id = '$ntrID'"); // Displays nutrition facts of roasted chicken with id n101
+        $_SESSION['drinks'] = []; // Set an array to store all drinks orders in SESSION
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { // When a post is done above, execute code
+        
+        if(isset($_POST["count"])) {
+
+            $count = $_POST["count"];
+        
+            // Get max order ID
+            $maxOrderQuery = "SELECT MAX(CAST(SUBSTRING(ordr_id, 2) AS UNSIGNED)) AS max_orderID FROM order_table";
+            $maxResult = mysqli_query($conn, $maxOrderQuery);
+            $maxRow = mysqli_fetch_assoc($maxResult);
+            $maxOrderID = $maxRow['max_orderID'];
+        
+            $orderID = "o" . ($maxOrderID + 1);
+        
+            $drinkID = $_POST["item"]; // Get $drinkID
+        
+            $order = new Order($orderID); 
+            $order->setOrder($count, $drinkID); // Set new order
+          }
+        
+        
+        if(isset($_POST["submit"])) {
+            $ordr_id = $order->getOrderID();
+            $quantity = $order->getQuantity();
+            $item_id = $order->getItemID();
+        
+            error_reporting(E_ERROR | E_PARSE);
             
-            $factsResult = mysqli_fetch_assoc($factsQuery);
+            $insert = "INSERT INTO order_table VALUES ('$ordr_id', '$quantity', '$item_id')";
+            mysqli_query($conn, $insert);
 
-            return $factsResult['desc'];
-        }
+            // Update stocks table based on item ID with order quantity
+            $updateDrinkStock = "UPDATE stocks s
+                                    JOIN drinks d ON s.stocks_id = d.stocks_id
+                                    SET s.quantity = s.quantity - $quantity
+                                    WHERE d.drinks_id = '$item_id'";
+            mysqli_query($conn, $updateDrinkStock); 
 
-        function getDrinkName($conn, $drinkID) {
+            array_push($_SESSION['drinks'],$ordr_id); // Add order IDs to drinks array in session
 
-            $nameQuery = mysqli_query($conn, "SELECT names FROM drinks WHERE drinks_id = '$drinkID'");
-
-            $nameResult = mysqli_fetch_assoc($nameQuery);
-
-            return $nameResult['names'];
-        }
-
-        function getDrinkPrice($conn, $drinkID) {
-
-            $priceQuery = mysqli_query($conn, "SELECT price FROM drinks WHERE drinks_id = '$drinkID'");
-
-            $priceResult = mysqli_fetch_assoc($priceQuery);
-
-            return "PHP " . $priceResult['price'] . ".00";
-        }
-
-        function getDrinkFacts($conn, $ntrID) {
-
-            $factsQuery = mysqli_query($conn, "SELECT * FROM nutr_facts WHERE nutr_facts_id = '$ntrID'"); 
+            echo "Record has been successfully inserted!";
             
-            while ($factsResult = mysqli_fetch_assoc($factsQuery)) {
-                
-                echo "Ingredients: ". $factsResult ["Ingredients"], "<br><br>";
-                echo "Fat: ". $factsResult ["Fat"], "g<br>";
-                echo "Calories: ". $factsResult ["Calories"], "g<br>";
-                echo "Carbs: ". $factsResult ["Carbs"], "g<br>";
-                echo "Protein: ". $factsResult ["Protein"], "g<br>";
+            } else {
+                echo "Failed to insert record!!!";
             }
+    }
+
+    function getDrinkDesc($conn, $ntrID) {
+    
+        $factsQuery = mysqli_query($conn, "SELECT * FROM nutr_facts WHERE nutr_facts_id = '$ntrID'"); // Displays nutrition facts of roasted chicken with id n101
+        
+        $factsResult = mysqli_fetch_assoc($factsQuery);
+
+        return $factsResult['desc'];
+    }
+
+    function getDrinkName($conn, $drinkID) {
+
+        $nameQuery = mysqli_query($conn, "SELECT names FROM drinks WHERE drinks_id = '$drinkID'");
+
+        $nameResult = mysqli_fetch_assoc($nameQuery);
+
+        return $nameResult['names'];
+    }
+
+    function getDrinkPrice($conn, $drinkID) {
+
+        $priceQuery = mysqli_query($conn, "SELECT price FROM drinks WHERE drinks_id = '$drinkID'");
+
+        $priceResult = mysqli_fetch_assoc($priceQuery);
+
+        return "PHP " . $priceResult['price'] . ".00";
+    }
+
+    function getDrinkFacts($conn, $ntrID) {
+
+        $factsQuery = mysqli_query($conn, "SELECT * FROM nutr_facts WHERE nutr_facts_id = '$ntrID'"); 
+        
+        while ($factsResult = mysqli_fetch_assoc($factsQuery)) {
+            
+            echo "Ingredients: ". $factsResult ["Ingredients"], "<br><br>";
+            echo "Fat: ". $factsResult ["Fat"], "g<br>";
+            echo "Calories: ". $factsResult ["Calories"], "g<br>";
+            echo "Carbs: ". $factsResult ["Carbs"], "g<br>";
+            echo "Protein: ". $factsResult ["Protein"], "g<br>";
         }
-    ?>
+    }
+?>
 </body>
 </html>
